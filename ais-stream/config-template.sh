f83c87097e69c3ce8f733af02e750f65decf6306
pwoods25443 <pwoods25443@gmail.com>
2020-08-29 20:12:56 -0400

    update ais-stream config template

--- ais-stream/config-template.sh
+++ ais-stream/config-template.sh
@@ -6,17 +6,18 @@ GCP_PROJECT=[GCP Project ID]
 #TODO: Set service account.  The service account should have Pub/Sub Publisher and BigQuery Data Editor roles
 SERVICE_ACCOUNT=[service account email]
 
+PREFIX=dev-test-ais-stream
 
-PREFIX=dev-test-
+NMEA_PUBSUB_TOPIC=projects/${GCP_PROJECT}/topics/${PREFIX}-nmea
+NMEA_PUBSUB_SUBSCRIPTION=projects/${GCP_PROJECT}/subscriptions/${PREFIX}-nmea-debug
+NMEA_CLOUDFUNC=${PREFIX}-nmea
 
-NMEA_PUBSUB_TOPIC=projects/${GCP_PROJECT}/topics/${PREFIX}ais-stream-nmea
-NMEA_PUBSUB_SUBSCRIPTION=projects/${GCP_PROJECT}/topics/${PREFIX}ais-stream-nmea-debug
-NMEA_CLOUDFUNC=${PREFIX}ais-stream-nmea
+DECODE_PUBSUB_TOPIC=projects/${GCP_PROJECT}/topics/${PREFIX}-decode
+DECODE_PUBSUB_SUBSCRIPTION=projects/${GCP_PROJECT}/subscriptions/${PREFIX}-decode-debug
+DECODE_CLOUDFUNC=${PREFIX}-decode
 
-DECODE_PUBSUB_TOPIC=projects/${GCP_PROJECT}/topics/${PREFIX}ais-stream-decode
-DECODE_PUBSUB_SUBSCRIPTION=projects/${GCP_PROJECT}/topics/${PREFIX}ais-stream-decode-debug
-DECODE_CLOUDFUNC=${PREFIX}ais-stream-decode
-
-BIGQUERY_TABLE=${PREFIX//-/_}ais_stream_raw_decoded
+BIGQUERY_DATASET=${GCP_PROJECT}:${PREFIX//-/_}
+BIGQUERY_TABLE=${BIGQUERY_DATASET}.raw_decoded
+BIGQUERY_CLOUDFUNC=${PREFIX}-bqstore
 
 DEFAULT_SOURCE=${PREFIX}ais-stream-nmea
