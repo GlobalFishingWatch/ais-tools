ad1fee15aaed358b487dd52fe8663c4dbc9e2792
pwoods25443 <pwoods25443@gmail.com>
2025-02-26 13:57:13 -0500

    comment code changes for clarity

--- ais_tools/ais5.py
+++ ais_tools/ais5.py
@@ -4,6 +4,9 @@ from ais import DecodeError
 
 def ais5_decode(body, pad):
     try:
+        # NB: we ignore any extra bits in the body, so we only take the first 71 characters
+        # because libais will raise an exception if the body with pad is not exactly 424 bits.
+        # 6 bits per character less the 2 padding bits (71 * 6 - 2 = 424)
         return libais.decode(body[:71], 2)
     except DecodeError as e:
         raise DecodeError(f'TYPE 5 LIBAIS ERR: {str(e)}')
